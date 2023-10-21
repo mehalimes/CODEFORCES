a8a89f9ac9db0969686806a4ca45d1cf6a1b9165
Mehmet Ali MEŞE <147969652+mehalimes@users.noreply.github.com>
2023-10-21 11:03:17 +0300

    Add files via upload

---
+++ PoweringTheHero(easy).cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+// C1 Powering the hero (easy version)
+// IDEA : DCC NITA Youtube
+// Author : mehalimes
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            for(int tc = 0; tc < t; tc++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+                long[] ar = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt64);
+                ArrayList sorted_ar = new ArrayList();
+                long total = 0L;
+                for(int i = 0; i < ar.Length; i++)
+                {
+                    if (ar[i] == 0 && sorted_ar.Count > 0)
+                    {
+                        sorted_ar.Sort();
+                        total += Convert.ToInt64(sorted_ar[sorted_ar.Count - 1]);
+                        sorted_ar.RemoveAt(sorted_ar.Count - 1);
+                    }
+                    else
+                    {
+                        sorted_ar.Add(ar[i]);
+                    }
+                }
+                Console.WriteLine(total);
+            }
+            //Console.ReadLine();
+            return;
+        }
+    }
+}
+
