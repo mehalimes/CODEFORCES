15cb0cdd07743333c1c05e0121db07574950b4f4
Mehmet Ali MEŞE <147969652+mehalimes@users.noreply.github.com>
2023-10-24 13:39:29 +0300

    Add files via upload

---
+++ NewYearCandles379A.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Split(' ');
+            int a = Convert.ToInt32(tokens[0]);
+            int b = Convert.ToInt32(tokens[1]);
+            int usedCandleCount = 0;
+            while(a >= b)
+            {
+                int div = a / b;
+                usedCandleCount += div * b;
+                a -= div * b;
+                a += div;
+            }
+            usedCandleCount += a;
+            Console.WriteLine(usedCandleCount);
+            //Console.ReadLine();
+        }
+    }
+}
