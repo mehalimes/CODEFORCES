fcd2d7a3a111381b3ffe9ca290766beb8ad3579e
Mehmet Ali MEŞE <147969652+mehalimes@users.noreply.github.com>
2023-10-25 13:34:54 +0300

    Add files via upload

---
+++ KDivisibleSum1476A.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+// author: mehalimes
+// K-divisible Sum Codeforces Solution 1476A
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            for(int tc = 0; tc < t; tc++)
+            {
+                string[] tokens = Console.ReadLine().Split(' ');
+                long n, k;
+                n = Convert.ToInt64(tokens[0]);
+                k = Convert.ToInt64(tokens[1]);
+                if(k == 1)
+                {
+                    Console.WriteLine(1);
+                    continue;
+                }
+                if(n <= k)
+                {
+                    long ans = k / n;
+                    if(k % n == 0)
+                    {
+                        ;
+                    }
+                    else
+                    {
+                        ans += 1;
+                    }
+                    Console.WriteLine(ans);
+                }
+                else if(n > k)
+                {
+                    long dest = Convert.ToInt64(n / k);
+                    if(n % k != 0)
+                    {
+                        dest++;
+                    }
+                    k = dest * k;
+                    long ans = k / n;
+                    if(k % n == 0)
+                    {
+                        ;
+                    }
+                    else
+                    {
+                        ans += 1;
+                    }
+                    Console.WriteLine(ans);
+                }
+            }
+            Console.ReadLine();
+            return;
+        }
+    }
+}
