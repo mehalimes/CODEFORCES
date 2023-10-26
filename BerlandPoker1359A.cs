a19c936b1014aee681cc7b78b9fd8bd9c5a29e80
Mehmet Ali MEŞE <147969652+mehalimes@users.noreply.github.com>
2023-10-26 16:11:54 +0300

    Add files via upload

---
+++ BerlandPoker1359A.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+// author: mehalime
+// Berland Poker 1359A Codeforces Solution
+// IDEA : Editorial
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            for(int tc = 0; tc < t; tc++)
+            {
+                string[] tokens = Console.ReadLine().Split(' ');
+                int n = Convert.ToInt32(tokens[0]);
+                int m = Convert.ToInt32(tokens[1]);
+                int k = Convert.ToInt32(tokens[2]);
+                int cardPerPerson = n / k;
+                long mx = long.MinValue;
+                if(m <= cardPerPerson)
+                {
+                    mx = m;
+                    Console.WriteLine(mx);
+                }
+                else if(m > cardPerPerson)
+                {
+                    mx = cardPerPerson;
+                    m -= cardPerPerson;
+                    long ans = 0L;
+                    if(m % (k-1) == 0)
+                    {
+                        ans = m / (k - 1);
+                    }
+                    else if(m % (k-1) != 0)
+                    {
+                        ans = (m / (k - 1)) + 1;
+                    }
+                    Console.WriteLine(mx - ans);
+                }
+            }
+            Console.ReadLine();
+            return;
+        }
+    }
+}
