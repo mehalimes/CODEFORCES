4e6ba50f838063e96a50bc8e19748e42d7377eb8
Mehmet Ali MEŞE <147969652+mehalimes@users.noreply.github.com>
2023-10-29 10:06:55 +0300

    Add files via upload

---
+++ IlyaAndQueries313B.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define l long 
+#define pb push_back
+#define pf push_front
+
+// author : mehalimes
+// IDEA : Editorial
+
+int main(){
+    using namespace std;
+    string s; cin >> s;
+    ll q; cin >> q;
+    vector<pair<ll, ll>> vp;
+    for(ll i = 0; i < q; i++){
+        ll x, y; cin >> x >> y;
+        vp.pb({x,y});
+    }
+    vector<ll> pref;
+    pref.pb(0);
+    for(ll i = 0; i+1 < (ll)s.length(); i++){
+        if(s[i] == s[i+1]){
+            pref.pb(1);
+        }
+        else {
+            pref.pb(0);
+        }
+    }
+    for(ll i = 1; i < (ll)pref.size(); i++){
+        pref[i] += pref[i-1];
+    }
+    for(ll i = 0; i < (ll)vp.size(); i++){
+        cout << pref[vp[i].second-1]-pref[vp[i].first-1] << "\n";
+    }
+    return 0;
+}
+
